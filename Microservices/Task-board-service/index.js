65cdc85a2bb0356c1e58dac6a794b2736835f3db
Dziarmaga, Aleksy <aleksy.dziarmaga@gmail.com>
2017-11-15 19:56:45 +0100

    Add simple funcionality for emitting task

--- Microservices/Task-board-service/index.js
+++ Microservices/Task-board-service/index.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer();
-const io = require('socket.io')(server);
 
 const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/task', (req, res) => {
+    const task = req.body;
+    res.send(task);
+    io.emit('taskAdded', task);
+
+})
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
