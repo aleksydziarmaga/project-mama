308f701043d7882e48433f749f0e079ea0ca546a
meheheu <meHeHeu@gmail.com>
2017-11-27 07:15:11 +0100

    modified script creating database to match diagram, added fiels for future use.

--- mamadb_CREATE.sql
+++ mamadb_CREATE.sql
@@ -2,33 +2,33 @@ CREATE DATABASE mama;
 
 USE mama;
 
-CREATE TABLE categories (
-id          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name        VARCHAR(20)
-);
-
-CREATE TABLE category_task (
-category_id INT UNSIGNED NOT NULL REFERENCES categories(id),
-task_id     INT UNSIGNED NOT NULL REFERENCES tasks(id)
-);
-
-CREATE TABLE tasks (
-id          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-user_id     INT UNSIGNED NOT NULL REFERENCES user(id),
-name        VARCHAR(20)  NOT NULL,
-description TEXT,
-state       ENUM('inactive', 'active', 'finished') NOT NULL DEFAULT 'inactive'
-);
-
-CREATE TABLE category_user (
-category_id INT UNSIGNED NOT NULL REFERENCES categories(id),
-user_id     INT UNSIGNED NOT NULL REFERENCES users(id)
-);
-
 CREATE TABLE users (
 id       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name     VARCHAR(30)  NOT NULL UNIQUE,
 mail     VARCHAR(30)  NOT NULL UNIQUE,
 password VARCHAR(30)  NOT NULL
+);
+
+CREATE TABLE category_user (
+uid INT UNSIGNED NOT NULL REFERENCES users(id),
+cid INT UNSIGNED NOT NULL REFERENCES categories(id)
+);
+
+CREATE TABLE categories (
+id          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name        VARCHAR(20)  NOT NULL UNIQUE
+);
+
+CREATE TABLE tasks_categories (
+tid INT UNSIGNED NOT NULL REFERENCES tasks(id),
+cid INT UNSIGNED NOT NULL REFERENCES categories(id)
+);
+
+CREATE TABLE tasks (
+id          INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+author_id   INT UNSIGNED NOT NULL REFERENCES user(id),
+name        VARCHAR(20)  NOT NULL,
+description TEXT,
+state       ENUM('inactive', 'active', 'finished') NOT NULL DEFAULT 'inactive'
 );
 
