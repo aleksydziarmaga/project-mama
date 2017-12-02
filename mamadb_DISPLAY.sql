b833e5c5137a307c7281d75d1233ea2a9eee69b9
meheheu <meheheu@gmail.com>
2017-12-02 09:41:01 +0100

    Added quick display.

---
+++ mamadb_DISPLAY.sql
@@ -0,0 +1,8 @@
+USE mama;
+
+SELECT id, name FROM users;
+SELECT id, name FROM categories;
+SELECT id, name FROM tasks;
+SELECT *        FROM users_categories;
+SELECT *        FROM tasks_categories;
+
