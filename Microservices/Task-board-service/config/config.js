976b0b846095c2e6a6e0de580d2434d91dba43b6
Dziarmaga, Aleksy <aleksy.dziarmaga@gmail.com>
2018-01-16 18:34:51 +0100

    Update database config

--- Microservices/Task-board-service/config/config.js
+++ Microservices/Task-board-service/config/config.js
@@ -3,6 +3,7 @@ require('dotenv').config({path: __dirname + '/.env'});
 const config = {}
 
 config.database = {
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
