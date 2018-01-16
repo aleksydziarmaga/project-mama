fa07418affe52d9098f9a4cd81fb5a8eb39fccc5
Dziarmaga, Aleksy <aleksy.dziarmaga@gmail.com>
2018-01-16 19:48:04 +0100

    Add get user tasks

--- Microservices/Task-board-service/src/routes/tasks.js
+++ Microservices/Task-board-service/src/routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {addTask, getTasks} = require('../controllers/tasks');
+const {addTask, getTasks, getUserTasks} = require('../controllers/tasks');
 
 router.get('/', getTasks);
+router.get('/:id', getUserTasks);
 router.post('/', addTask);
 
 module.exports = router;
