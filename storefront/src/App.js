38eb8be9c9b8c61c3db499f3b9c5f902f4d3cb02
Dziarmaga, Aleksy <aleksy.dziarmaga@sap.com>
2018-01-29 08:46:11 +0100

    Add react router

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css'; 
 import Board from './Board'; 
 import Form from './Form'; 
@@ -6,10 +7,12 @@ import Form from './Form';
 class App extends Component { 
   render() { 
     return ( 
-    	<div>
-    	  {/* <Form /> */}
-        <Board />
-      </div>
+    	<Router>
+        <div>
+    	    <Route path='/tasks' component ={Form} />
+          <Route path='/board' component={Board}/>
+        </div>
+      </Router>
       ); 
   } 
 } 
